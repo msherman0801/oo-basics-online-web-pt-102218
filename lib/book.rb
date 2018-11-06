730a3d34eb0df2905bbdfef9e4ea649af1cc6868
Matthew Sherman <msherman0801@gmail.com>
2018-11-06 22:55:38 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -4,14 +4,10 @@ class Book
     @title = title
   end
   
-  def title
-    @title
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
   
   attr_accessor :author, :page_count, :genre
-  
+  attr_reader :title
 end
